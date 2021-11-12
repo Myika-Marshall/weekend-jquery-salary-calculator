1ae046fcb8e9406a82c2c37a6e90c717a3fcfc6c
Myika Marshall <myika.marshall@gmail.com>
2021-11-12 12:30:25 -0600

    created functions and variables to calculate salary of employees

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
 $(document).ready(onReady);
+// create a global array to accept and hold employees in.
 
-let employee = {
+function onReady(){
+    console.log('in onReady');
+    $('#submit-button').on('click', employeeData);
+//     $('#employees-tbody').on('click', '.delete-btn', deleteButton);
+
+function deleteButton(){
+    $(this).closest('tr').remove();
+}
+
+
+const currentEmployees = [];
+
+function handleAddEmployeeClick () {
+const onboardEmployee = {
+    //assign the inputs a location
     firstName: $('#first-name').val(), 
     lastName: $('#last-name').val(),
     idNum:$('#id').val(),
     title: $('#title').val(),
     annualSalary:$('#annual-salary').val(),
+}
+// I need to add the new employees to my employee table
+currentEmployees.push(onboardEmployee);
 
+employeeData(currentEmployees);
+monthlySalaryCalculator(currentEmployees);
 }
 
-function onReady(){
-    console.log('in onReady');
-    $('#submit-button').on('click', employeeData)
-
+function renderEmployees() {
+    $('#employees-tbody').empty();
+    //clear out inputs
+    $('#first-name').val ('');
+    $('#last-name').val ('');
+    $('#id').val ('');
+    $('#title').val ('');
+    $('#annual-salary').val ('');
 }
+
+
 function employeeData(employees){
     console.log('in engageEmployee');
 // I want to create an append that adds employee data to my table
-    for (let empoyee of employess){
-        $('#tbody').append(
+    // for (let empoyee of currentEmployess){
+        $('#employees-tbody').append(
     `<tr>
-    <td>${employee.firstName}</td>  
-    <td>${employee.lastname}</td>
-    <td>${employee.idNum}</td>
-    <td>${employee.title}</td>
-    <td>${employee.annualSalary}/td>
-    <td>empty text</td>
+        <td>${onboardEmployee.firstName}</td>  
+        <td>${onboardEmployee.lastname}</td>
+        <td>${onbaordEmployee.idNum}</td>
+        <td>${onboardEmployee.title}</td>
+        <td>${onboardEmployee.annualSalary}/td>
+        <td>empty text</td>
     </tr>`);
-    }
-// I need to calculate the Annual Salaries
-// I need to indicate if the calculation takes me over 20,000
-} 
+        }
+}
+// // I need to calculate the Annual Salaries
+// // I need to indicate if the calculation takes me over 20,000 (red)
+function updateMonthlySalary(employees){
+    let annualTotal = 0;
+    let monthlyTotal = annualTotal / 12;
 
+    for (let employee of employees){
+        annualSalary += employee.annualSalary;
+    } if (monthlyTotal > 20000) {
+        $('#total-monthly-h3').addClass('red-background');
+    } else if ($('#total-monthly-h3').removeClass('red-background')
+}
+
+// let roundedTotalMonthly = (monthlyTotal).toFixed(2);
+// $('#total-monthly-cost').text(roundedTotalMonthly);
+
+// //I need to access the delete button
